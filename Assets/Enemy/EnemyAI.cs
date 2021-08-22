06ef40370ff73b346e7bc3dd02d488d1e9c82f6e
Szukács Marcell <spiglebach@gmail.com>
2021-08-22 17:11:54 +0200

    enemy provocation

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -4,18 +4,39 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour {
     [SerializeField] private Transform target;
     [SerializeField] private float chaseRange = 5f;
+    [SerializeField] private float attackCooldownInSeconds = 3f;
 
     private NavMeshAgent navMeshAgent;
+
+    private bool isProvoked = false;
+    private float distanceToTarget = Mathf.Infinity;
+    private float remainingAttackCooldown;
 
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     void Update() {
-        float distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= chaseRange) {
+        remainingAttackCooldown -= Time.deltaTime;
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        isProvoked = isProvoked || distanceToTarget <= chaseRange;
+        if (isProvoked) {
+            EngageTarget();
+        }
+    }
+
+    private void EngageTarget() {
+        if (distanceToTarget <= navMeshAgent.stoppingDistance) {
+            Attack();
+        } else {
             navMeshAgent.SetDestination(target.position);
         }
+    }
+
+    private void Attack() {
+        if (remainingAttackCooldown > 0) return;
+        Debug.Log("You have been attacked!");
+        remainingAttackCooldown = attackCooldownInSeconds;
     }
 
     private void OnDrawGizmosSelected() {
